24056cc9c9e2c1b742e866e062feb7585d8e95c4
Adella <adellakenn@gmail.com>
2019-06-23 02:07:00 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -5,6 +5,8 @@ class MusicLibraryController
   end
   
   def call
+        input = nil
+      while input != "exit"
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
     puts "To list all of the artists in your library, enter 'list artists'."
@@ -14,25 +16,10 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-        input = nil
-      while input != "exit"
         input = gets.chomp
       end
       
-      case input
-        when "list songs"
-          self.list_songs
-        when "list artists"
-          self.list_artists
-        when "list genres"
-          self.list_genres
-        when "list artist"
-          self.list_songs_by_artist
-        when "list genre"
-          self.list_songs_by_genre
-        when "play song"
-          self.play_song
-      end 
+      
    end
    
    # "1. Thundercat - For Love I Come -dance"
