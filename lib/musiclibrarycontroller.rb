7af6b705a33685fa0f5fe4b587e6b39e04a03412
Adella <adellakenn@gmail.com>
2019-06-23 02:07:14 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -18,7 +18,20 @@ class MusicLibraryController
     puts "What would you like to do?"
         input = gets.chomp
         
-        
+         case input
+        when "list songs"
+          self.list_songs
+        when "list artists"
+          self.list_artists
+        when "list genres"
+          self.list_genres
+        when "list artist"
+          self.list_songs_by_artist
+        when "list genre"
+          self.list_songs_by_genre
+        when "play song"
+          self.play_song
+      end 
       end
       
       
